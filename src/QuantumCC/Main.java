2d523c814f03aeccde200deafa6ff8ad239e378b
ElsaKrasniqii <elsa.krasniqi12@student.uni-pr.edu>
2025-10-27 20:33:09 +0100

    rregullimi struktures

--- src/QuantumCC/Main.java
+++ src/QuantumCC/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args){
         System.out.println("Hello Elsa!");
         System.out.println("ELONA");
+        System.out.println("PROVA");
     }
 }
