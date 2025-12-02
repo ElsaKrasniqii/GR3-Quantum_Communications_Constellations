f4ad3e8e5e4a51bcac4e012db1c7ab922fd65adb
Elsa Krasniqi <elsa.krasniqi12@student.uni-pr.edu>
2025-12-02 23:11:50 +0100

    Importimi i librarive te duhura

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print("Hello")
+#!/usr/bin/env python3
+"""
+Quantum Communications Constellations Optimizer
+Main entry point for the optimization problem
+SPOC-2 Challenge: Quantum Communications Constellations
+"""
+
+
+import sys
+import os
+import argparse
+import time
+from datetime import datetime
+
