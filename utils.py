c1de3fa0f2b69182b70b7469cd13d1c1a5dd38cd
ElonaKuqiiii <elona.kuqi@student.uni-pr.edu>
2025-12-03 20:29:29 +0100

    Commit 1: Initial Setup

---
+++ utils.py
@@ -0,0 +1,12 @@
+# utils.py - Version 1
+import numpy as np
+import json
+from datetime import datetime
+import warnings
+warnings.filterwarnings('ignore')
+
+__version__ = "1.0.0"
+__author__ = "Quantum Communications Team"
+__date__ = "2025"
+
+print("Initializing utils module...")
